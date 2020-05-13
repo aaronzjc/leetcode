b495e917c4517815eae520f426f22673613578e3
jincheng <415397228@qq.com>
2020-05-14 00:12:01 +0800

    roman to int

---
+++ easy/RomanToInteger.go
@@ -0,0 +1,44 @@
+package easy
+
+// https://leetcode-cn.com/problems/roman-to-integer/
+
+func romanToInt(s string) (res int) {
+	m := map[string]int{
+		"I": 1,
+		"IV": 4,
+		"V": 5,
+		"IX": 9,
+		"X": 10,
+		"XL": 40,
+		"L": 50,
+		"XC": 90,
+		"C": 100,
+		"CD": 400,
+		"D": 500,
+		"CM": 900,
+		"M": 1000,
+	}
+	var c, n string
+	i, l, rs := 0, len(s)-1, []byte(s)
+	for i <= l {
+		if n != "" {
+			c = n
+		} else {
+			c = string(rs[i])
+		}
+		i++
+		if i > l {
+			res += m[c]
+			continue
+		}
+		n = string(rs[i])
+		if r, ok := m[c+n]; ok {
+			res += r
+			n = ""
+			i++
+			continue
+		}
+		res += m[c]
+	}
+	return
+}
---
+++ easy/RomanToInteger_test.go
@@ -0,0 +1,27 @@
+package easy
+
+import "testing"
+
+func TestRomanToInteger(t *testing.T)  {
+	seeds := []struct{
+		input string
+		expect int
+	} {
+		{"III", 3},
+		{"IV", 4},
+		{"IX", 9},
+		{"X", 10},
+		{"LVIII", 58},
+		{"MCMXCIV", 1994},
+	}
+
+	for _, v := range seeds {
+		result := romanToInt(v.input)
+		if result != v.expect {
+			t.Error(v.input, result, v.expect)
+			t.Fatalf("failed !")
+		}
+	}
+
+	t.Log("passed !")
+}
