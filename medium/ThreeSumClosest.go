8f8452ff4884d704702502f6c6e531ddbaaa73fe
jincheng <415397228@qq.com>
2020-05-16 17:04:48 +0800

    three sum closest

---
+++ medium/ThreeSumClosest.go
@@ -0,0 +1,57 @@
+package medium
+
+import (
+	"fmt"
+	"sort"
+)
+
+// https://leetcode-cn.com/problems/3sum-closest/
+
+func abs(n int) int {
+	if n > 0 {
+		return n
+	}
+	return -1 * n
+}
+
+func threeSumClosest(nums []int, target int) (res int) {
+	sort.Ints(nums)
+	var i, j, k, minus, s int
+	l := len(nums) - 1
+	fmt.Println(nums)
+	pre := -1
+	for i <= l {
+		j = i + 1
+		for j <= l {
+			k = j + 1
+			for k <= l {
+				s = nums[i]  + nums[j] + nums[k]
+				minus = abs(s - target)
+				if minus == 0 {
+					res = s
+					return
+				}
+				if pre < 0 {
+					pre = minus
+					res = s
+					k++
+					continue
+				}
+				if minus < pre {
+					pre = minus
+					res = s
+				}
+				k++
+			}
+			j++
+			for j < l && nums[j] == nums[j-1] {
+				j++
+			}
+		}
+		i++
+		for i < l && nums[i] == nums[i-1] {
+			i++
+		}
+	}
+	return
+}
---
+++ medium/ThreeSumClosest_test.go
@@ -0,0 +1,26 @@
+package medium
+
+import "testing"
+
+func TestThreeSumClosest(t *testing.T) {
+	seeds := []struct{
+		input []int
+		target int
+		expect int
+	} {
+		{[]int{-1,2,1,-4}, 1, 2},
+		{[]int{1,1,1,1}, 0, 3},
+		{[]int{1,1,-1,-1,3}, 1, 1},
+		{[]int{1,1,-1,-1,3}, 3, 3},
+	}
+
+	for _, v := range seeds {
+		result := threeSumClosest(v.input, v.target)
+		if result != v.expect {
+			t.Error(v.input, v.target, v.expect, result)
+			t.Fatalf("failed !")
+		}
+	}
+
+	t.Log("ThreeSumClosest passed !")
+}
