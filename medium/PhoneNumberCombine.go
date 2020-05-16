4ab18c4420b0e09ba301a36ea13bcaddcc973ef4
jincheng <415397228@qq.com>
2020-05-16 17:48:14 +0800

    phone number combination

---
+++ medium/PhoneNumberCombine.go
@@ -0,0 +1,38 @@
+package medium
+
+// https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+
+var (
+	m = map[string][]string {
+		"2": {"a", "b", "c"},
+		"3": {"d", "e", "f"},
+		"4": {"g", "h", "i"},
+		"5": {"j", "k", "l"},
+		"6": {"m", "n", "o"},
+		"7": {"p", "q", "r", "s"},
+		"8": {"t", "u", "v"},
+		"9": {"w", "x", "y", "z"},
+	}
+)
+
+func letterCombinations(digits string) (res []string) {
+	var i, l int
+	for _, v := range []byte(digits) {
+		vs := string(v)
+		vsv := m[vs]
+		if len(res) == 0 {
+			res = vsv
+			continue
+		}
+		i = 0
+		l = len(res)
+		for i < l {
+			for _, vv := range vsv {
+				res = append(res, res[i]+vv)
+			}
+			i++
+		}
+		res = res[i:]
+	}
+	return
+}
---
+++ medium/PhoneNumberCombine_test.go
@@ -0,0 +1,26 @@
+package medium
+
+import (
+	"github.com/aaronzjc/leetcode/tools"
+	"testing"
+)
+
+func TestLettersCombination(t *testing.T) {
+	seeds := []struct{
+		input string
+		expect []string
+	} {
+		{"3", []string{"d", "e", "f"}},
+		{"32", []string{"da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"}},
+	}
+
+	for _, v := range seeds {
+		result := letterCombinations(v.input)
+		if !tools.IsStringArrEquals(result, v.expect, false) {
+			t.Error(v.input, v.expect, result)
+			t.Fatalf("failed !")
+		}
+	}
+
+	t.Log("LettersCombination passed !")
+}
