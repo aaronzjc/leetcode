5399c6ab259de1490dd1cde1e0fbcb2fa38907db
jincheng <415397228@qq.com>
2020-07-04 21:07:36 +0800

    jump game two

--- hard/JumpGameTwo.go
+++ hard/JumpGameTwo.go
@@ -2,6 +2,33 @@ package hard
 
 // https://leetcode-cn.com/problems/jump-game-ii/
 
-func jump(nums []int) int {
+// TIPS:
+// 贪心算法。每一次往前跳的时候，都选择最优的。对于这个题目，每次跳选择跳完后索引最远的。
 
+func jump(nums []int) (res int) {
+	l := len(nums) - 1
+	var i, j, max, maxIdx int
+	for i <= l {
+		if i == l {
+			break
+		}
+		if i+nums[i] >= l {
+			res++
+			break
+		}
+		max = -1
+		maxIdx = -1
+		for j = 0; j <= nums[i]; j++ {
+			if nums[i+j]+j >= max {
+				max = nums[i+j] + j
+				maxIdx = j
+			}
+		}
+
+		if maxIdx > -1 {
+			i = i + maxIdx
+			res++
+		}
+	}
+	return
 }
--- hard/JumpGameTwo_test.go
+++ hard/JumpGameTwo_test.go
@@ -2,18 +2,25 @@ package hard
 
 import "testing"
 
-func TestJumpGameTwo(t *testing.T)  {
-	seeds := []struct{
-		input []int
+func TestJumpGameTwo(t *testing.T) {
+	seeds := []struct {
+		input  []int
 		expect int
-	} {
-		{[]int{2,3,1,4}, 2},
+	}{
+		{[]int{0}, 0},
+		{[]int{1, 2, 1, 1, 1}, 3},
+		{[]int{2, 3, 1, 4}, 2},
+		{[]int{2, 3, 1, 2, 4, 2, 3}, 3},
+		{[]int{1, 1, 1}, 2},
+		{[]int{4, 1, 1, 3, 1, 1, 1}, 2},
+		{[]int{10, 1, 1}, 1},
+		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0}, 2},
 	}
 
 	for _, v := range seeds {
 		result := jump(v.input)
 		if result != v.expect {
-			t.Errorf(v, result)
+			t.Error(v, result)
 			t.Fatalf("failed !")
 		}
 	}
