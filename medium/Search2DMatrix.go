cffd03752a6b94ebf5fc92e5741e5ecbbab02b7c
jincheng <415397228@qq.com>
2020-11-18 17:31:16 +0800

    search 2d matrix

---
+++ medium/Search2DMatrix.go
@@ -0,0 +1,37 @@
+package medium
+
+// https://leetcode-cn.com/problems/search-a-2d-matrix/
+
+// Tips:
+// 有点像二维空间下的二分查找，要做好地址映射
+
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	if len(matrix[0]) == 0 {
+		return false
+	}
+
+	rows := len(matrix)
+	cols := len(matrix[0])
+
+	left, right := 0, rows * cols - 1
+	mid := (left + right) / 2
+
+	for left <= right {
+		y := mid / cols
+		x := mid - y * cols
+
+		if matrix[y][x] == target {
+			return true
+		}
+		if matrix[y][x] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+		mid = (left + right) / 2
+	}
+	return false
+}
---
+++ medium/Search2DMatrix_test.go
@@ -0,0 +1,28 @@
+package medium
+
+import "testing"
+
+func TestSearchMatrix(t *testing.T) {
+	seeds := []struct{
+		matrix [][]int
+		target int
+		expect bool
+	} {
+		{[][]int{{1,3,5,7},{10,11,16,20},{23,30,34,50}},3, true},
+		{[][]int{{1,3,5,7},{10,11,16,20},{23,30,34,50}},13, false},
+		{[][]int{}, 0, false},
+		{[][]int{{}}, 0, false},
+		{[][]int{{1}}, 2, false},
+		{[][]int{{3}}, 2, false},
+	}
+
+	for _, v := range seeds {
+		result := searchMatrix(v.matrix, v.target)
+		if result != v.expect {
+			t.Error(v, result)
+			t.Fatalf("failed !")
+		}
+	}
+
+	t.Log("Search2DMatrix passed !")
+}
