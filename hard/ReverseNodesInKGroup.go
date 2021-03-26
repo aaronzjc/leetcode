4f6d838947203ea17fd29b5c514c9a078ed33c4b
jincheng <jincheng3@staff.weibo.com>
2021-03-26 16:39:21 +0800

    imp reverse k in group

--- hard/ReverseNodesInKGroup.go
+++ hard/ReverseNodesInKGroup.go
@@ -1,71 +1,66 @@
 package hard
 
 import (
+	"fmt"
 	"github.com/aaronzjc/leetcode/tools"
 )
 
 // https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
 
 // Tips:
-// 硬解。将链表根据k组，首先看是否有k个元素，如果没有，退出；如果有k个元素，则参考反转链表，将k个元素反转。然后拼接首尾。
-// 主要是边界条件判断，链表的边界很烦。
+// 暴力解法，写一个子函数，反转K个链表。然后，遍历每K个一组反转。
 
 func reverseKGroup(head *tools.ListNode, k int) (res *tools.ListNode) {
-	check := func(node *tools.ListNode, k int) (match bool, first *tools.ListNode, last *tools.ListNode) {
-		if node == nil {
-			return
-		}
-		first = node
-		last = node
-		for node != nil && k > 1 {
-			k--
-			node = node.Next
-			last = node
-		}
-		if k == 1 && node != nil {
-			match = true
-		}
+	if k == 1 {
+		res = head
 		return
 	}
-	reverse := func(head *tools.ListNode, k int) (start *tools.ListNode, end *tools.ListNode) {
+	var reverseList func(start *tools.ListNode, end *tools.ListNode) (newStart *tools.ListNode, newEnd *tools.ListNode)
+	reverseList = func(start *tools.ListNode, end *tools.ListNode) (newStart *tools.ListNode, newEnd *tools.ListNode) {
+		newEnd = start
+		newStart = end
 		var pre, tmp *tools.ListNode
-		start = head
-		end = head
-		i := k
-		for i > 1 {
+		for start != end {
+			fmt.Println(start.Val)
+			if pre == nil {
+				pre = start
+				start = start.Next
+				continue
+			}
 			tmp = start.Next
 			start.Next = pre
 			pre = start
 			start = tmp
-			i--
 		}
 		start.Next = pre
 		return
 	}
 
-	if head == nil {
-		return
-	}
-	var start, end, ln, pre *tools.ListNode
-	counter := 0
-	res = head
-	for {
-		match, first, last := check(head, k)
-		if !match {
-			break
+	var s, e, ne *tools.ListNode
+	i := 1
+	for head != nil {
+		if i%k == 1 {
+			s = head
 		}
-		ln = last.Next
-		start, end = reverse(first, k)
-		if counter == 0 {
-			res = start
+		if i%k == 0 {
+			e = head
+
+			// 重置分组迭代
+			i = 1
+			head = head.Next
+
+			if ne != nil {
+				ne.Next = e
+			}
+			if res == nil {
+				res = e
+			}
+			_, ne = reverseList(s, e)
+			ne.Next = head
+			continue
 		}
-		if pre != nil {
-			pre.Next = start
-		}
-		pre = end
-		end.Next = ln
-		head = end.Next
-		counter++
+		i++
+		head = head.Next
 	}
 	return
 }
