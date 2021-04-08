152228c63ec3fb80a4b0ea9c3187e66c2b8b2764
jincheng <jincheng3@staff.weibo.com>
2021-04-08 15:30:37 +0800

    coin change

--- golang/ConsisHash.go
+++ golang/ConsisHash.go
@@ -10,18 +10,18 @@ import (
 // 一致性hash
 type HashFunc func([]byte) uint32
 
-type ConsisHash struct {
+type ConsistHash struct {
 	hash  HashFunc
 	Reps  int   // 副本数
 	Nodes []int // 排序后的节点
 	M     map[int]string
 }
 
-func NewConsistHash(reps int, fn HashFunc) *ConsisHash {
+func NewConsistHash(reps int, fn HashFunc) *ConsistHash {
 	if fn == nil {
 		fn = crc32.ChecksumIEEE
 	}
-	return &ConsisHash{
+	return &ConsistHash{
 		hash: fn,
 		Reps: reps,
 		M:    make(map[int]string),
@@ -29,7 +29,7 @@ func NewConsistHash(reps int, fn HashFunc) *ConsisHash {
 }
 
 // Add 添加节点
-func (hs *ConsisHash) Add(nodes ...string) {
+func (hs *ConsistHash) Add(nodes ...string) {
 	for _, node := range nodes {
 		for i := 0; i < hs.Reps; i++ {
 			hash := int(hs.hash([]byte(node + strconv.Itoa(i))))
@@ -42,7 +42,7 @@ func (hs *ConsisHash) Add(nodes ...string) {
 }
 
 // Get 根据key返回合适的节点
-func (hs *ConsisHash) Get(key string) string {
+func (hs *ConsistHash) Get(key string) string {
 	if len(hs.Nodes) == 0 {
 		return ""
 	}
